7ed527402e5e6d42d0960f27c91b1302466b6ab0
[VasilJivkov] <vasil_jivkov@hotmail.com>
2018-03-21 12:28:36 +0200

    added seeders for categories

--- models/seeders/20180320215341-categories.js
+++ models/seeders/20180320215341-categories.js
@@ -3,9 +3,62 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Categories', [{
-      name: 'Electronics',
-      createdAt: new Date(),
-      updatedAt: new Date(),
+        name: 'Electronics',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: 'Auto',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: '101 sessions',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: 'Home Furniture',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: 'Pets',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },    {
+        name: 'Tools',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },    {
+        name: 'Sports',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },    {
+        name: 'Office',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },{
+        name: 'Games',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },{
+        name: 'Fashion',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },{
+        name: 'Services',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },{
+        name: 'Real Estate',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        name: 'Health',
+        createdAt: new Date(),
+        updatedAt: new Date(),
     }], {});
   },
 
