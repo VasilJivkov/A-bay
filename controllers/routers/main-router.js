0ae8ad7ed38f32e2fb244d955e5c51873de509d3
Milena Atanasova <atamasowa40@gmail.com>
2018-03-27 15:26:24 +0300

    Reordered the rotes, removed dublicated css-reset, minor pug changes.

--- controllers/routers/main-router.js
+++ controllers/routers/main-router.js
@@ -5,41 +5,39 @@ const {
 const init = (app, data) => {
     const router = new Router();
 
-    router.get('/', (req, res) => {
-        console.log('Routes!!!');
+    router
+        .get('/', (req, res) => {
+            console.log('Routes!!!');
 
-        res.render('_shared/master');
-    });
+            res.render('_shared/master');
+        })
+        .get('/signin', (req, res) => {
+            res.render('forms/signin');
+        })
+        .get('/adds', (req, res) => {
+            const adds = [{
+                title: 'ThinkPad T55',
+                price: 300 + 'lv',
+                picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
+            }, {
+                title: 'ThinkPad T55',
+                price: 300 + 'lv',
+                picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
+            }, {
+                title: 'ThinkPad T55',
+                price: 300 + 'lv',
+                picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
+            }];
 
-    router.get('/signin', (req, res) => {
-        res.render('forms/signin');
-    });
+            const content = {
+                adds,
+            };
 
-    router.get('/adds', (req, res) => {
-        const adds = [{
-            title: 'ThinkPad T55',
-            price: 300 + 'lv',
-            picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
-        }, {
-            title: 'ThinkPad T55',
-            price: 300 + 'lv',
-            picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
-        }, {
-            title: 'ThinkPad T55',
-            price: 300 + 'lv',
-            picture: 'https://smartphone.bg/system/images/151631/normal/MQAC2GHA.jpg',
-        }];
-
-        const content = {
-            adds,
-        };
-
-        res.render('products/list', content);
-    });
-
-    router.get('/categories', (req, res) => {
-        res.render('products/categories');
-    });
+            res.render('products/list', content);
+        })
+        .get('/categories', (req, res) => {
+            res.render('products/categories');
+        });
 
     app.use('/', router);
 };
