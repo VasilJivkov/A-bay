acc6df9023e19e43984b6588d4058de44c32db5e
Milena Atanasova <atamasowa40@gmail.com>
2018-03-31 02:47:04 +0300

    Some cool hints

--- controllers/data/generic.js
+++ controllers/data/generic.js
@@ -13,15 +13,34 @@ class Data {
         return this.Model.findAll();
     }
 
+    /**
+    * @description Get row from table by given id.
+    * @param id number
+    * @return Returns element with the id parameter.
+    */
+
     getById(id) {
         return this.Model.findById(id, {
             include: this.includes,
         });
     }
 
+    /**
+   * @description Create new object in the database
+   * @param dataObj object
+   * @return Returns the created element.
+   */
+
     create(obj) {
         return this.Model.create(obj);
     }
+
+    /**
+   * @description Get row from table by given id.
+   * @param id number
+   * @param data array form objects with arttribute and value to update
+   * @return Returns db object with id = id parameter.
+   */
 
     update(id, data) {
         const dBTableUpdate = (key, value) => {
@@ -47,11 +66,23 @@ class Data {
         });
     }
 
+    /**
+     * @description Constructor for Sequelize queries for filter.
+     * @param object to filter by
+     * @return Returns array with matching rows form this.Model.
+     */
+
     filterConstructor(filterObj) {
         return this.Model.findAll({
             where: filterObj,
         });
     }
+
+    /**
+        * @description Filter method, which filters by city.
+        * @param id number
+        * @return returns array with matching rows form this.Model.
+        */
 
     filterByCity(id) {
         return this.filterConstructor({
@@ -59,17 +90,36 @@ class Data {
         });
     }
 
+    /**
+       * @description Filter method, which filters by category.
+       * @param id number
+       * @return returns array with matching rows form this.Model.
+       */
+
     filterByCategory(id) {
         return this.filterConstructor({
             fk_category_id: id,
         });
     }
 
+    /**
+           * @description Filter method, which filters by user.
+           * @param id number
+           * @return returns array with matching rows form this.Model.
+           */
+
     filterByUser(id) {
         return this.filterConstructor({
             fk_user_id: id,
         });
     }
+
+    /**
+     * @description Order by given column and value.
+     * @param column string
+     * @param parameter string
+     * @return Returns ordered Model.
+     */
 
     orderBy(column, parameter) {
         return this.Model.findAll({
@@ -79,13 +129,27 @@ class Data {
         });
     }
 
+    /**
+     * @description Search by given column and value.
+     * @param category string,
+     * @param parameter string,
+     * @return Returns array with all of the data from this.Model
+     *  where category&&parameter matches something
+     */
+
     searchBy(catecogy, parameter) {
         return this.Model.findAll({
             where: {
-                catecogy: parameter,
+                category: parameter,
             },
         });
     }
+
+    /**
+     * @description Find custom user by given id.
+     * @param username string
+     * @return Returns custom user with all of his data from the Users model
+     */
 
     findByUsername(username) {
         return this.Model.findOne({
@@ -94,6 +158,13 @@ class Data {
             },
         });
     }
+
+    /**
+     * @description Gets all of the CreatedAt values of this.Model
+     * @async
+     * @param no params
+     * @return Returns array with all of the CreatedAt values of this.Model
+     */
 
     async getAllCreatedAdDates() {
         const users = await this.getAll();
